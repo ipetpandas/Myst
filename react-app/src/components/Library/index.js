3ae48f5f74db52e6a7271f630d215f3ca86eb4ac
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-28 14:03:47 -0800

    library progress

--- react-app/src/components/Library/index.js
+++ react-app/src/components/Library/index.js
@@ -9,6 +9,7 @@ import "./Library.css";
 
 const Library = () => {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
   const library = useSelector((state) => state.library);
   let [isLoaded, setIsLoaded] = useState(false);
 
@@ -16,42 +17,71 @@ const Library = () => {
     dispatch(thunkReadUserLibrary()).then(() => setIsLoaded(true));
   }, [dispatch]);
   return (
-    <div className="entire-library-container">
-      <div className="library-sidebar-parent">
-        <div className="library-sidebar-container">
-          {isLoaded &&
-            Object.values(library).map((game) => {
-              return (
-                <div className="library-game-list-container">{game.title}</div>
-              );
-            })}
-        </div>
-      </div>
-      <div className="library-parent">
-        <div className="library-header">Your Games</div>
-        <div className="library-container">
-          {isLoaded &&
-            Object.values(library).map((game) => {
-              return (
-                <div className="library-game-container">
-                  <img
-                    className="library-banner"
-                    src={game.main_banner_url}
-                  ></img>
-                  <div className="library-info-container">
-                    <div className="library-game-title">{game.title}</div>
-                    <div className="library-links-container">
-                      <button className="library-game-button">Game Page</button>
-                      <button className="library-review-button">Review</button>
+    user && (
+      <div className="entire-library-parent">
+        <div className="entire-library-container">
+          <div className="library-sidebar-parent">
+            <div className="library-sidebar-container">
+              <div className="my-games-header">
+                <i className="fa-solid fa-minus"></i>
+                &nbsp;My Games&nbsp;
+                <span className="my-games-count">
+                  ({Object.keys(library).length})
+                </span>
+              </div>
+              {isLoaded &&
+                Object.values(library).map((game) => {
+                  return (
+                    <NavLink exact to={`/games/${game.id}`}>
+                      <div className="library-game-list-container">
+                        {game.title}
+                      </div>
+                    </NavLink>
+                  );
+                })}
+            </div>
+          </div>
+          <div className="library-parent">
+            <div className="library-header">ALL GAMES</div>
+            <div className="library-container">
+              {isLoaded &&
+                Object.values(library).map((game) => {
+                  return (
+                    <div className="library-game-container">
+                      <img
+                        className="library-banner"
+                        src={game.main_banner_url}
+                      ></img>
+                      <div className="library-info-container">
+                        <div className="library-game-title-container">
+                          <div className="library-game-title">{game.title}</div>
+                          <div className="library-game-purchase">
+                            Purchased: {dateConverter(game.created_at)}
+                          </div>
+                        </div>
+                        <div className="library-links-container">
+                          <button className="library-button">More Info</button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
+                  );
+                })}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
+};
+
+const dateConverter = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "utc",
+  });
 };
 
 export default Library;
