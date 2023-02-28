a8ed30349d1918243fab910475ab24a0e0b1d14a
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-28 10:00:10 -0800

    library layout

--- react-app/src/components/Library/index.js
+++ react-app/src/components/Library/index.js
@@ -5,6 +5,7 @@ import { thunkClearCart, thunkReadUserCart } from "../../store/carts";
 import { useSelector } from "react-redux";
 import "./Library.css";
 import { thunkReadUserLibrary } from "../../store/libraries";
+import "./Library.css";
 
 const Library = () => {
   const dispatch = useDispatch();
@@ -15,15 +16,40 @@ const Library = () => {
     dispatch(thunkReadUserLibrary()).then(() => setIsLoaded(true));
   }, [dispatch]);
   return (
-    <div className="library-container">
-      {isLoaded &&
-        Object.values(library).map((game) => {
-          return (
-            <div>
-              <img src={game.main_banner_url}></img>
-            </div>
-          );
-        })}
+    <div className="entire-library-container">
+      <div className="library-sidebar-parent">
+        <div className="library-sidebar-container">
+          {isLoaded &&
+            Object.values(library).map((game) => {
+              return (
+                <div className="library-game-list-container">{game.title}</div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="library-parent">
+        <div className="library-header">Your Games</div>
+        <div className="library-container">
+          {isLoaded &&
+            Object.values(library).map((game) => {
+              return (
+                <div className="library-game-container">
+                  <img
+                    className="library-banner"
+                    src={game.main_banner_url}
+                  ></img>
+                  <div className="library-info-container">
+                    <div className="library-game-title">{game.title}</div>
+                    <div className="library-links-container">
+                      <button className="library-game-button">Game Page</button>
+                      <button className="library-review-button">Review</button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
